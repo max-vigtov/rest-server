009b70de2c6cb1bf9ffa1f1612465f266f73ef39
Max Vigueras <mvt.2000@hotmail.com>
2025-02-06 20:35:56 -0600

    SPA: Serve spa with router.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 export class Server {
 
@@ -12,6 +13,11 @@ export class Server {
 		//* Public Folder
 		this.app.use( express.static( 'public' ) )
 
+		this.app.get('*', (req, res) => {
+			const indexPath = path.join(__dirname + '../../../public/index.html')
+			res.sendFile(indexPath)
+			return;
+		})
 
 		this.app.listen(3000, () => {
 			console.log(`Server is running on ${3000}`)
