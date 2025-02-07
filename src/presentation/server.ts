584919678c29a34bca770ead9803685900e1639a
Max Vigueras <mvt.2000@hotmail.com>
2025-02-06 22:24:46 -0600

    Update: Package scripts updated.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -34,7 +34,7 @@ export class Server {
 		})
 
 		this.app.listen(this.port, () => {
-			console.log(`Server is running on ${ this.port }`)
+			console.log(`Server is running on port ${ this.port }`)
 		})
 	}
 }
