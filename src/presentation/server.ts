f4ea89c50ddd5a037a4dd291bdd607b908e0294f
Max Vigueras <mvt.2000@hotmail.com>
2025-02-06 21:46:02 -0600

    Env: Enviroment config added.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,23 @@
 import express from 'express';
 import path from 'path';
 
+interface Options {
+	port: number;
+	public_path?: string;
+}
+
 export class Server {
 
 	private app = express()
+	private readonly port: number;
+	private readonly publicPath: string;
+
+	constructor( options: Options ) {
+		const { port, public_path = 'public'} = options;
+		this.port = port;
+		this.publicPath = public_path;
+
+	}
 
 	async start(){
 		
@@ -11,16 +25,16 @@ export class Server {
 		
 
 		//* Public Folder
-		this.app.use( express.static( 'public' ) )
+		this.app.use( express.static( this.publicPath ) )
 
 		this.app.get('*', (req, res) => {
-			const indexPath = path.join(__dirname + '../../../public/index.html')
+			const indexPath = path.join(__dirname + `../../../${ this.publicPath }/index.html`)
 			res.sendFile(indexPath)
 			return;
 		})
 
-		this.app.listen(3000, () => {
-			console.log(`Server is running on ${3000}`)
+		this.app.listen(this.port, () => {
+			console.log(`Server is running on ${ this.port }`)
 		})
 	}
 }
